00e1245277a90135c5fbff63d41da6e8e8be7f82
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-10 14:14:18 -0400

    Aula 3 - Atividade 3 criando o SetSaldo

--- src/curso_java2/Conta.java
+++ src/curso_java2/Conta.java
@@ -3,6 +3,9 @@ package curso_java2;
 public class Conta {
 	protected double saldo;
 	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 	public double getSaldo() {
 		return this.saldo;
 	}
