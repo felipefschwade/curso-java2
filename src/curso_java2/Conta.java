ec604af70d6d8f0f7d45fb38c43ebdaf753aa959
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-05-09 23:34:37 -0400

    Aula 2 - Atividades 1 e 2 Criando a classe conta e implementando o método atualiza

--- src/curso_java2/Conta.java
+++ src/curso_java2/Conta.java
@@ -1,5 +1,18 @@
 package curso_java2;
 
 public class Conta {
-
+	private double saldo;
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	public void saca(double valor) {
+		this.saldo -= valor;
+	}
+	public void atualiza(double taxa) {
+		this.saldo += (this.saldo * taxa);
+	}
 }
